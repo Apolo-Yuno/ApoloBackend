0190a21788d1eddf9e220b13bc5b87ea9641d8d4
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-12-13 13:27:32 -0500

    add(core): Añadiendo conexion con cluster y configuracion de swagger

--- src/main/java/com/hackathon/yuno/controller/MerchantController.java
+++ src/main/java/com/hackathon/yuno/controller/MerchantController.java
@@ -4,14 +4,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.yuno.model.dto.response.MerchantResponseDTO;
+
 @RequestMapping ("/merchant")
 @RestController
 public class MerchantController {
 
     @PostMapping("")
     public MerchantResponseDTO createMerchant(){
-
-        
+        return null;
 
     }
 
