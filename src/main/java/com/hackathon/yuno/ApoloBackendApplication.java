1cf160d2c1803624039b073dfe1af79491439bfd
Tulio Riano Sanchez <tulio.riano-s@mail.escuelaing.edu.co>
2025-12-13 12:24:50 -0500

    feat(core): Initial Structure

---
+++ src/main/java/com/hackathon/yuno/ApoloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.yuno.ApoloBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/yuno/ApoloBackend/ApoloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.yuno.ApoloBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
